a31245953e9aebe4b9812e486897e86d7c611346
Martina Fogliato <martinafogliato@gmail.com>
2018-04-04 16:53:25 +0200

    add comments

--- Lab2/work/lab2.c
+++ Lab2/work/lab2.c
@@ -1,28 +1,28 @@
 // Include files for SSE4
-#include "emmintrin.h"
-#include "xmmintrin.h"
+#include <emmintrin.h>
+#include <xmmintrin.h>
 #include <stdint.h>
-
+#include <tmmintrin.h>
 // example SIMD macros, not necessary to be used, write your own
 
+__m128i mmtmpb;
 inline void cmult(__m128i a,__m128i b, __m128i *re32, __m128i *im32) {
 
-  mmtmpb    = _mm_sign_epi16(b,*(__m128i*)reflip);
-  *re32     = _mm_madd_epi16(a,mmtmpb);
-  mmtmpb    = _mm_shufflelo_epi16(b,_MM_SHUFFLE(2,3,0,1));
-  mmtmpb    = _mm_shufflehi_epi16(mmtmpb,_MM_SHUFFLE(2,3,0,1));
+  mmtmpb    = _mm_sign_epi16(b,*(__m128i*)reflip); //Negates packed words in a if corresponding sign in b is less than zero. Interpreting a, b, and r as arrays of signed 16-bit integers
+  *re32     = _mm_madd_epi16(a,mmtmpb); //Multiplies 128x128
+  mmtmpb    = _mm_shufflelo_epi16(b,_MM_SHUFFLE(2,3,0,1)); //Shuffles the lower four signed or unsigned 16-bit integers in a as specified by imm. The shuffle value, imm, must be an immediate. _MM_SHUFFLE(2, 3, 0, 1) permutes [a3, a2, a1, a0] to [a2, a3, a0, a1]. 
+  mmtmpb    = _mm_shufflehi_epi16(mmtmpb,_MM_SHUFFLE(2,3,0,1)); //Shuffles the upper four signed or unsigned 16-bit integers in a as specified by imm. The shuffle value, imm, must be an immediate.
   *im32  = _mm_madd_epi16(a,mmtmpb);
-
 }
 
 __m128i cpack_tmp1,cpack_tmp2;
 inline __m128i cpack(__m128i xre,__m128i xim) {
 
-  cpack_tmp1 = _mm_unpacklo_epi32(xre,xim);
-  cpack_tmp1 = _mm_srai_epi32(cpack_tmp1,15);
+  cpack_tmp1 = _mm_unpacklo_epi32(xre,xim); //Unpack and interleave 32-bit integers from the low half of a and b, and store the results in dst. 
+  cpack_tmp1 = _mm_srai_epi32(cpack_tmp1,15); //Shift packed 32-bit integers in 'a' right by imm while shifting in sign bits, and store the results in dst. 
   cpack_tmp2 = _mm_unpackhi_epi32(xre,xim);
-  cpack_tmp2 = _mm_srai_epi32(cpack_tmp2,15);
-  return(_mm_packs_epi32(cpack_tmp1,cpack_tmp2));
+  cpack_tmp2 = _mm_srai_epi32(cpack_tmp2,15); 
+  return(_mm_packs_epi32(cpack_tmp1,cpack_tmp2)); //Convert packed 32-bit integers from a and b to packed 16-bit integers using signed saturation
 
 }
 
@@ -30,7 +30,7 @@ inline __m128i cpack(__m128i xre,__m128i xim) {
 void componentwise_multiply_real_scalar(int16_t *x,int16_t *y,int16_t *z,uint16_t N) {
 
 }
-_
+
 // routines to be written
 void componentwise_multiply_real_sse4(int16_t *x,int16_t *y,int16_t *z,uint16_t N) {
 
@@ -40,6 +40,6 @@ __m128i *z128 = (__m128i *)z;
  
 }
 
-main() {
-
+int main() {
+return 0;
 }
--- Lab2/work/time_meas.h
+++ Lab2/work/time_meas.h
@@ -10,22 +10,26 @@ typedef struct {
   int trials;
 } time_stats_t;
 
-static inline void start_meas(time_stats_t *ts) __attribute__((always_inline));
+static inline void start_meas(time_stats_t *ts) __attribute__((always_inline)); //forces the function to be inlined ("inlined functions are as fast as macros")
 static inline void stop_meas(time_stats_t *ts) __attribute__((always_inline));
 
 #if defined(__i386__)
 static inline unsigned long long rdtsc_oai(void) __attribute__((always_inline));
 static inline unsigned long long rdtsc_oai(void) {
     unsigned long long int x;
-    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x)); //volatile on __asm__ forces the compiler to execute code as-is, otherwise it may think it can be removed or lifted out of a loop and cached
+	//opcode of rdtsc is F 31
     return x;
 }
 #elif defined(__x86_64__)
 static inline unsigned long long rdtsc_oai() __attribute__((always_inline));
 static inline unsigned long long rdtsc_oai() { 
   unsigned long long a, d;
-  __asm__ volatile ("rdtsc" : "=a" (a), "=d" (d));
-  return (d<<32) | a;
+  __asm__ volatile ("rdtsc" : "=a" (a), "=d" (d)); //volatile prevents caching, otherwise the compiler is allowed to cache the timestamp while we need a fresh one every time
+  //a=low part; d=high part
+  //need to force serialization as well?
+ //The Time Stamp Counter (TSC) is a 64-bit register present on all x86 processors since the Pentium. It counts the number of cycles since reset. The instruction RDTSC returns the TSC in EDX:EAX
+  return (d<<32) | a; //on 64bit machine, rdtsc clears the higher 32 bits of RAX, to compensate we need to shift left by 32 the higher part
 }
 #endif
 
@@ -34,7 +38,7 @@ static inline void start_meas(time_stats_t *ts) {
 #ifdef OMP
   int tid;
 
-  tid = omp_get_thread_num();
+  tid = omp_get_thread_num(); //get number of threads used in a parallel region
   if (tid==0)
 #endif
     {
